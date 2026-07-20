6aaadbef91868b1a5469d6639cd1ad44d687c065
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-20 22:35:17 +0530

    added scalekit

---
+++ src/lib/scalekit.ts
@@ -0,0 +1,9 @@
+import { ScalekitClient } from '@scalekit-sdk/node';
+
+const scalekit = new ScalekitClient(
+  process.env.SCALEKIT_ENVIRONMENT_URL!,
+  process.env.SCALEKIT_CLIENT_ID!,
+  process.env.SCALEKIT_CLIENT_SECRET!
+);
+
+export default scalekit;
